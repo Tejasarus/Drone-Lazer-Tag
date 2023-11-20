a58ac25f6a43bb8c390fe5a3feca442c79417923
Tejas Shah <50884660+Tejasarus@users.noreply.github.com>
2023-11-19 16:41:31 -0800

    Add files via upload

--- vision.py
+++ vision.py
@@ -7,22 +7,33 @@ rval, frame = vc.read()
 pink = (255, 153, 255)
 
 while True:
-   hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+  #define frame
+  hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+  
+  #define color range (high and low)
+  lower_red = np.array([155,65,57])
+  upper_red = np.array([255,255,180])
 
-   lower_red = np.array([30,150,50])
-   upper_red = np.array([255,255,180])
-    
-   mask = cv2.inRange(hsv, lower_red, upper_red)
-   res = cv2.bitwise_and(frame,frame, mask= mask)
+  #draw frame with only color, black and white
+  mask = cv2.inRange(hsv, lower_red, upper_red)
+  res = cv2.bitwise_and(frame,frame, mask= mask)
 
-   if frame is not None:
-     cv2.imshow("preview", frame)
-     cv2.imshow('mask',mask)
-     cv2.imshow('res',res)
-   rval, frame = vc.read()
+  #find color in original frame
+  colorcnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
 
-   if cv2.waitKey(1) & 0xFF == ord('q'):
-     break
+  #draw box around color
+  if len(colorcnts) > 0:
+      color_area = max(colorcnts, key=cv2.contourArea)
+      (xg,yg,wg,hg) = cv2.boundingRect(color_area)
+      cv2.rectangle(frame,(xg,yg),(xg+wg, yg+hg),(0,255,0),2)
 
+  #display colors
+  if frame is not None:
+    cv2.imshow("preview", frame)
+    cv2.imshow('mask',mask)
+    cv2.imshow('res',res)
+  rval, frame = vc.read()
 
-#pink 255, 153, 255
+  #press q to quit program
+  if cv2.waitKey(1) & 0xFF == ord('q'):
+    break
