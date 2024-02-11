efbb5c0763ce03c2fb511078d03eb86953ddc930
Tejas Shah <tejas@Tejass-Air-4.lan>
2024-02-10 17:09:27 -0800

    vision class

--- vision.py
+++ vision.py
@@ -1,39 +1,47 @@
 import cv2
 import numpy as np
-cv2.namedWindow("preview")
-vc = cv2.VideoCapture(0)
 
-rval, frame = vc.read()
-pink = (255, 153, 255)
+class Vision:
+    def __init__(self,rval,vc,frame,height,width):
+        self.frame = frame
+        self.height = height
+        self.width = width
+        self.detected = False
+        self.vc = vc
+        self.rval = rval
 
-while True:
-  #define frame
-  hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-  
-  #define color range (high and low)
-  lower_pink = np.array([140, 50, 50])
-  upper_pink = np.array([180, 255, 255])
+    def see(self):
+        #define frame
+        hsv = cv2.cvtColor(self.frame, cv2.COLOR_BGR2HSV)
+        
+        #define color range (high and low)
+        lower_pink = np.array([140, 50, 50])
+        upper_pink = np.array([180, 255, 255])
 
-  #draw frame with only color, black and white
-  mask = cv2.inRange(hsv, lower_pink, upper_pink)
-  res = cv2.bitwise_and(frame,frame, mask= mask)
+        #draw frame with only color, black and white
+        mask = cv2.inRange(hsv, lower_pink, upper_pink)
+        res = cv2.bitwise_and(self.frame,self.frame, mask= mask)
 
-  #find color in original frame
-  colorcnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
+        #find color in original frame
+        colorcnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
+        #check if drone is in center of screen (fire range)
+        if len(colorcnts) > 0:
+            color_area = max(colorcnts, key=cv2.contourArea)
+            (xg,yg,wg,hg) = cv2.boundingRect(color_area)
+            #print((xg,yg,wg,hg))
+            cv2.rectangle(self.frame,(xg,yg),(xg+wg, yg+hg),(0,255,0),2)
+            if xg <= self.width/2 and yg <= self.height/2 and yg + hg >= self.height/2 and xg + wg >= self.width/2:
+                self.detected = True
+            else:
+                self.detected = False
 
-  #draw box around color
-  if len(colorcnts) > 0:
-      color_area = max(colorcnts, key=cv2.contourArea)
-      (xg,yg,wg,hg) = cv2.boundingRect(color_area)
-      cv2.rectangle(frame,(xg,yg),(xg+wg, yg+hg),(0,255,0),2)
+        #display colors
+        #if self.frame is not None:
+            #cv2.imshow("preview", self.frame)
+            #cv2.imshow('mask',mask)
+            #cv2.imshow('res',res)
 
-  #display colors
-  if frame is not None:
-    cv2.imshow("preview", frame)
-    cv2.imshow('mask',mask)
-    cv2.imshow('res',res)
-  rval, frame = vc.read()
+        self.rval,self.frame = self.vc.read()
 
-  #press q to quit program
-  if cv2.waitKey(1) & 0xFF == ord('q'):
-    break
+    def is_detected(self):
+        return self.detected
